314408731ed4f20b56f84d085681a0e0d382f96d
Felipe Matarazzo <felipemps@protonmail.com>
2021-04-27 23:07:58 -0300

    reduced time complexity

--- two_strings/two_strings.rb
+++ two_strings/two_strings.rb
@@ -1,11 +1,9 @@
 def two_strings(s1, s2)
   answer = "NO"
-  s1.split("").each do |l1|
-    s2.split("").each do |l2|
-      l1 == l2 ? answer = "YES" : answer
-      s1.delete(l1)
-      s2.delete(l2)
-    end
+  s1_hash = {}
+  s1.split("").each_with_index { |l1, index1| s1_hash[index1] = l1 }
+  s2.split("").each_with_index do |l2, index2|
+    answer = "YES" if s1[index2] == l2
   end
   answer
 end
