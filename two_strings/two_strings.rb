a72bdf1f4549d3c3868aa9fc81a5c4f0bc291559
Felipe Matarazzo <felipemps@protonmail.com>
2021-04-28 12:06:12 -0300

    algorithm improved

--- two_strings/two_strings.rb
+++ two_strings/two_strings.rb
@@ -1,9 +1,4 @@
 def two_strings(s1, s2)
-  answer = "NO"
-  s1_hash = {}
-  s1.split("").each_with_index { |l1, index1| s1_hash[index1] = l1 }
-  s2.split("").each_with_index do |l2, index2|
-    answer = "YES" if s1[index2] == l2
-  end
-  answer
+  equivalent = s1.split("").filter { |l| s2.include?(l)}
+  equivalent.length > 0 ? "YES" : "NO"
 end
